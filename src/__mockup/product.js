10a37874604cdf66c1f52fa42d8dcc32a3653170
nam181196 <102805613+nam181196@users.noreply.github.com>
2022-12-19 19:30:32 +0700

    Add files via upload

--- src/__mockup/product.js
+++ src/__mockup/product.js
@@ -1,1 +1,30 @@
-
+const products = [
+  {
+    id: 1,
+    images: [
+      "http://placekitten.com/400/252",
+      "http://placekitten.com/200/126",
+      "http://placekitten.com/200/126",
+    ],
+    name: "Kitten",
+    description:
+      "Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.",
+    price: "180",
+    sizes: ["S", "M", "L"],
+    colors: ["black", "white", "yellow"],
+  },
+  {
+    id: 2,
+    images: [
+      "http://placekitten.com/400/126",
+      "http://placekitten.com/200/126",
+    ],
+    name: "Kitten",
+    description:
+      "Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.",
+    price: "200",
+    sizes: ["S", "M", "L"],
+    colors: ["black", "white", "yellow", "#0ff0cf"],
+  },
+];
+export default products;
